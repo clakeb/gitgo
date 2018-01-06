b4183d37a6c5ecdb3ced8a22ba9cf5236d171877
Blake Barber <blakebarber@protonmail.com>
2018-01-05 20:05:04 -0600

    First Go app. Followed tutorial from @petermbenjamin on [Medium](https://medium.freecodecamp.org/writing-command-line-applications-in-go-2bc8c0ace79d).

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	flag "github.com/ogier/pflag"
+)
+
+var (
+	user string
+)
 
 func main() {
-	fmt.Println("Hello World")
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		printUsage()
+	}
+
+	if flag.NFlag() == 0 {
+		fmt.Printf("Usage: %s [options]\n", os.Args[0])
+		fmt.Println("Options:")
+
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	users := strings.Split(user, ",")
+	fmt.Printf("Searching user(s): %s\n", users)
+
+	for _, u := range users {
+		result := getUsers(u)
+		fmt.Println(`Username:	`, result.Login)
+		fmt.Println(`Name:		`, result.Name)
+		fmt.Println(`Email:		`, result.Email)
+		fmt.Println(`Bio:		`, result.Bio)
+		fmt.Println("")
+	}
+}
+
+func init() {
+	flag.StringVarP(&user, "user", "u", "", "Search Users")
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
